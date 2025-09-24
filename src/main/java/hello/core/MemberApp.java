597e8ca8784c74e97b5d80e67437e14c8ff66aee
waney <ssdii66@gmail.com>
2025-09-25 00:29:55 +0900

    bean/config 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberServiceImpl memberService = (MemberServiceImpl) appConfig.memberService();
 
-        MemberServiceImpl memberService = (MemberServiceImpl) appConfig.memberService();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = context.getBean("memberService", MemberServiceImpl.class);
+
+
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
 
